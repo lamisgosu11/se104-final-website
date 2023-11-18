298920d4f1485728e5f4169bca325c0af57c08f0
lam2 <lamq5qqqqq@gm.com>
2023-11-18 23:03:47 +0700

    Thêm comment cho định nghĩa hàm

--- admin_notification/views.py
+++ admin_notification/views.py
@@ -3,6 +3,9 @@ from admin_notification.models import Notification
 from django.conf import settings
 from django.contrib import admin
 
+
+#Hàm check_notification_view là một view function trong Django. 
+#Nó lấy đối tượng thông báo đầu tiên từ cơ sở dữ liệu, đặt trường count về 0, và sau đó chuyển hướng người dùng đến trang quản trị của mô hình thông báo dựa trên cài đặt đã xác định trước đó.
 def check_notification_view(request):
     instance = Notification.objects.all().first()
     instance.count = 0
