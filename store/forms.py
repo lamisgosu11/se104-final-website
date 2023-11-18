a6d0c498994d80a33edc3eb66f4fb8b3a2c89f14
KamDaXay <bleule26@gmail.com>
2023-11-18 22:41:39 +0700

    Tạo forms.py

---
+++ store/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Comment
+from django import forms
+
+
+
+from .models import Order
+
+
+
+
