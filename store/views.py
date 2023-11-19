8c728f1e868c7631f13a3724e1bd02630aae1e10
Marsen <lamisgosu11@gmail.com>
2023-11-19 21:23:15 +0700

    update views.py updateItem and processOrder

--- store/views.py
+++ store/views.py
@@ -149,3 +149,67 @@ def checkout(request):
         cartItems = order['get_cart_items']
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
+
+# updateItem view xử lý yêu cầu của người dùng để thêm hoặc xóa một sản phẩm trong giỏ hàng
+# -> sau đó trả về một JSON với thông tin về sản phẩm đã được thêm hoặc xóa
+# Lưu thay đổi vào csdl
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action : ', action)
+    print('productId : ', productId)
+
+    customer = request.user
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(
+        customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(
+        order=order, product=product)
+    if (action == 'add'):
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif (action == 'remove'):
+        orderItem.quantity = (orderItem.quantity - 1)
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+    return JsonResponse('Item was added', safe=False)
+
+# processOrder view xử lý yêu cầu của người dùng để thanh toán
+# -> kiểm tra người dùng -> Lấy đơn hàng hiện tại của người dùng hoặc tạo một đơn hàng mới nếu không tồn tại.
+# -> Lấy tổng số tiền của đơn hàng từ dữ liệu đã được chuyển đổi -> gán ID -> kiểm tra tiền -> lưu thay đổi vào csdl
+# -> kiểm tra thuộc tính của đơn hàng -> kiểm tra yêu cầu giao hàng -> tạo địa chỉ giao hàng mới
+
+
+def processOrder(request):
+    print('Data', request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+
+    else:
+        print('User is not logged in.. ')
+    return JsonResponse('Payment complete !', safe=False)
