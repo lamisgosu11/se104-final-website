29575907a01a238d3fccca5c455ef76d41af2ae7
NguyenHien <NguyenHien1307@users.noreply.github.com>
2023-11-20 17:29:55 +0700

    added "process_order", "home", "blog", "product"

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
     path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
+    path('home/', views.home, name="home"),
+    path('blog/', views.blog, name="blog"),
+    path('product/<str:slug>/', views.product, name="product"),
+    
 ]
