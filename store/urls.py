f1a7a84de1a3843fb935fc17b61d36aeddf23be1
NguyenHien <NguyenHien1307@users.noreply.github.com>
2023-11-20 17:25:46 +0700

    added 'cart', 'checkout' , 'update_item'

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.store, name = "store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('update_item/', views.updateItem, name="update_item"),
 ]
