50fed3e9f3311cf1004e2d9baa2f1630a84be94d
NguyenHien <NguyenHien1307@users.noreply.github.com>
2023-11-20 17:33:12 +0700

    added "blog_article", "contact", IDpages

--- store/urls.py
+++ store/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     path('home/', views.home, name="home"),
     path('blog/', views.blog, name="blog"),
     path('product/<str:slug>/', views.product, name="product"),
-    
+    path('blog_article/<str:slug>/', views.blog_article, name="blog_article"),
+    path('contact/', views.contact, name="contact"),
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', viewslogoutUser, name="logout"),
 ]
