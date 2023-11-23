dc66390f6d1191fd473c9d6f7ae7808fa282a70b
Phu <phthienphu1210@gmail.com>
2023-11-23 15:40:21 +0700

    Xây dựng lớp order để quản lý các yêu cầu đặt hàng và order item để ghi nhận các sản phẩm trong đơn hàng.

--- store/models.py
+++ store/models.py
@@ -37,3 +37,49 @@ class Product(models.Model):
         except: 
             url=''
         return url
+
+
+class Order(models.Model):
+    #a single customer can have multiple orders
+    customer = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    order_day = models.DateField(auto_now_add=True, null= True)
+    complete=models.BooleanField(default=False,null=True,blank=False)
+    transaction_id=models.CharField(max_length=100,null=True)  
+
+    def __str__(self):
+        if self.customer is not None:
+            return str(self.customer)
+        return "Unknown"
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+    @property
+    def shipping(self):
+        shipping=False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping=True
+            return shipping
+
+class OrderItem (models.Model):
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True,blank=True)
+    #order ynjm ykoun aando barsha orderitems
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True,blank=True)
+    quantity = models.IntegerField(default=0,null=True,blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
