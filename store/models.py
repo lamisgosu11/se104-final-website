50af1daef899630bc2284b3d3a15f11b3c269425
Phu <phthienphu1210@gmail.com>
2023-11-23 15:39:31 +0700

    Thêm class product

--- store/models.py
+++ store/models.py
@@ -16,3 +16,24 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name=models.CharField(max_length=200,null=True)   
+    price=models.DecimalField(max_digits=7,decimal_places=2) 
+    slug=models.SlugField(max_length=200,default="")
+    category=models.CharField(max_length=200,null=True) 
+    digital=models.BooleanField(default=False,null=True,blank=True)
+    image= models.ImageField(null=True,blank=True)
+    stock = models.IntegerField(null=True, blank=True)
+    stock_limit = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url=self.image.url
+        except: 
+            url=''
+        return url
