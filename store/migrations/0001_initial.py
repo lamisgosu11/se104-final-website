9085b5ebcdf0b4ab138014ba6e565906b8d31606
NguyenHien <NguyenHien1307@users.noreply.github.com>
2023-11-25 15:03:11 +0700

    Added Order and Post models for migrations

--- store/migrations/0001_initial.py
+++ store/migrations/0001_initial.py
@@ -20,4 +20,28 @@ class Migration(migrations.Migration):
                 ('user', models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)), 
             ],
         ),
+        migrations.CreateModel(
+            name= 'Order',
+            fields=[
+                ('id', models.AutoField(auto_created=True,primary_key=True, serialize=False, verbose_name='ID')),
+                ('date_ordered', models.DateTimeField(auto_now_add=True)),
+                ('complete', models.BooleanField(default=False, null=True)),
+                ('transaction_id', models.CharField(max_length=100, null=True)),
+                ('customer', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='store.customer')), 
+            ],
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=200, null=True)),
+                ('description', models.CharField(max_length=800, null=True)),
+                ('slug', models.SlugField(default='', max_length=200)),
+                ('date_posted', models.DateTimeField(auto_now_add=True)),
+                ('image', models.ImageField(blank=True, null=True, upload_to='')),
+            ],
+        ),
+        
     ]
+
+    
