4463102173d0ab3c273b488e8562a87d80f47b71
Phu <phthienphu1210@gmail.com>
2023-11-26 14:50:53 +0700

    fix bug cho hàm logout

--- store/urls.py
+++ store/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('contact/', views.contact, name="contact"),
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
-    path('logout/', viewslogoutUser, name="logout"),
+    path('logout/', views.logoutUser, name="logout"),
 ]
