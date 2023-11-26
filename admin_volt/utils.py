c8a31b59f196600991cf27714c8a9ac9fc794770
nkthang2003 <83719698+nkthang2003@users.noreply.github.com>
2023-11-27 00:46:48 +0700

    Update utils.py JsonResponse class

--- admin_volt/utils.py
+++ admin_volt/utils.py
@@ -39,3 +39,122 @@ except ImportError:
 default_apps_icon = {
     'auth': 'fa fa-users'
 }
+
+class JsonResponse(HttpResponse):
+    """
+    An HTTP response class that consumes data to be serialized to JSON.
+    :param data: Data to be dumped into json. By default only ``dict`` objects
+      are allowed to be passed due to a security flaw before EcmaScript 5. See
+      the ``safe`` parameter for more information.
+    :param encoder: Should be an json encoder class. Defaults to
+      ``django.core.serializers.json.DjangoJSONEncoder``.
+    :param safe: Controls if only ``dict`` objects may be serialized. Defaults
+      to ``True``.
+    """
+
+    def __init__(self, data, encoder=DjangoJSONEncoder, safe=True, **kwargs):
+        if safe and not isinstance(data, dict):
+            raise TypeError('In order to allow non-dict objects to be '
+                            'serialized set the safe parameter to False')
+        kwargs.setdefault('content_type', 'application/json')
+        data = json.dumps(data, cls=encoder)
+        super(JsonResponse, self).__init__(content=data, **kwargs)
+
+
+def get_app_list(context, order=True):
+    admin_site = get_admin_site(context)
+    request = context['request']
+
+    app_dict = {}
+    for model, model_admin in admin_site._registry.items():
+
+        app_icon = model._meta.app_config.icon if hasattr(
+            model._meta.app_config, 'icon') else None
+        app_label = model._meta.app_label
+        try:
+            has_module_perms = model_admin.has_module_permission(request)
+        except AttributeError:
+            has_module_perms = request.user.has_module_perms(
+                app_label)  # Fix Django < 1.8 issue
+
+        if has_module_perms:
+            perms = model_admin.get_model_perms(request)
+
+            # Check whether user has any perm for this module.
+            # If so, add the module to the model_list.
+            if True in perms.values():
+                info = (app_label, model._meta.model_name)
+                model_dict = {
+                    'name': capfirst(model._meta.verbose_name_plural),
+                    'object_name': model._meta.object_name,
+                    'perms': perms,
+                    'model_name': model._meta.model_name
+                }
+                if perms.get('change', False) or perms.get("view", False):
+                    try:
+                        model_dict['admin_url'] = reverse(
+                            'admin:%s_%s_changelist' % info, current_app=admin_site.name)
+                    except NoReverseMatch:
+                        pass
+                if perms.get('add', False):
+                    try:
+                        model_dict['add_url'] = reverse(
+                            'admin:%s_%s_add' % info, current_app=admin_site.name)
+                    except NoReverseMatch:
+                        pass
+                if app_label in app_dict:
+                    app_dict[app_label]['models'].append(model_dict)
+                else:
+                    try:
+                        name = apps.get_app_config(app_label).verbose_name
+                    except NameError:
+                        name = app_label.title()
+                    app_dict[app_label] = {
+                        'name': name,
+                        'app_label': app_label,
+                        'app_url': reverse(
+                            'admin:app_list',
+                            kwargs={'app_label': app_label},
+                            current_app=admin_site.name,
+                        ),
+                        'has_module_perms': has_module_perms,
+                        'models': [model_dict],
+                    }
+
+                if not app_icon:
+                    app_icon = default_apps_icon[app_label] if app_label in default_apps_icon else None
+                app_dict[app_label]['icon'] = app_icon
+
+    # Sort the apps alphabetically.
+    app_list = list(app_dict.values())
+
+    if order:
+        app_list.sort(key=lambda x: x['name'].lower())
+
+        # Sort the models alphabetically within each app.
+        for app in app_list:
+            app['models'].sort(key=lambda x: x['name'])
+
+    return app_list
+
+
+def get_admin_site(context):
+    try:
+        current_resolver = resolve(context.get('request').path)
+        index_resolver = resolve(
+            reverse('%s:index' % current_resolver.namespaces[0]))
+
+        if hasattr(index_resolver.func, 'admin_site'):
+            return index_resolver.func.admin_site
+
+        for func_closure in index_resolver.func.__closure__:
+            if isinstance(func_closure.cell_contents, AdminSite):
+                return func_closure.cell_contents
+    except:
+        pass
+
+    return admin.site
+
+
+def get_admin_site_name(context):
+    return get_admin_site(context).name
