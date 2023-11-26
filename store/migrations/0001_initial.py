49f51b8aa4749ceec33aa7b75aaace1fbb7fbfa7
NguyenHien <NguyenHien1307@users.noreply.github.com>
2023-11-26 09:45:29 +0700

    Added models for Product, ShipAdd, OrderItm, Cmt

--- store/migrations/0001_initial.py
+++ store/migrations/0001_initial.py
@@ -41,7 +41,52 @@ class Migration(migrations.Migration):
                 ('image', models.ImageField(blank=True, null=True, upload_to='')),
             ],
         ),
-        
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=200, null= True)),
+                ('price', models.DecimalField(decimal_places=2, max_digits=7)),
+                ('slug', models.SlugField(default='', max_length=200)),
+                ('category', models.CharField(max_length=200, null=True)),
+                ('digital', models.BooleanField(blank=True, default=False, null=True)),
+                ('image', models.ImageField(blank=True, null=True, upload_to='')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ShippingAddress',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length==200, null=True)),
+                ('address', models.CharField(max_length=200, null=True)),
+                ('city', models.CharField(max_length=200, null=True)),
+                ('state', models.CharField(max_length=200, null=True)),
+                ('zipcode', models.CharField(max_length=200, null=True)),
+                ('date_added', models.DateTimeField(auto_now_add=True)),
+                ('customer', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='store.customer')),
+                ('order', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='store.order')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='OrderItem',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('quantity', models.IntegerField(blank=True, default=0, null=True)),
+                ('date_added', models.DateTimeField(auto_now_add=True)),
+                ('order', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='store.order')),
+                ('product', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='store.product')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True,serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length==200)),
+                ('body', models.TextField(max_length=200)),
+                ('date_commented', models.DateTimeField(auto_now_add=True)),
+                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to='store.post')),
+            ],
+        ),
     ]
 
     
